0ccc5ba1972312c4df829ae670d90a159733780e
bibhas <bibhas2@gmail.com>
2019-08-13 15:10:19 -0400

    Initial commit

---
+++ simple_cnn.py
@@ -0,0 +1,51 @@
+# We will classify handwritten digits as 0 to 9. 
+# Source data MNIST
+
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+
+(train_images, train_labels), (test_images, test_labels) = datasets.mnist.load_data()
+
+# train_images and test_images are tensors of dimension m x 28 x 28.
+# Where m is the number of samples. Each image is 28x28 pixels.
+# Images are greyscale and hense has onely one color channel.
+
+# For CNN the input tensor must have a depth. We will just reshape the input
+# data to have 1 depth (since its greyscale). For color images the depth will be 3.
+image_width = train_images.shape[1]
+image_height = train_images.shape[2]
+
+train_images = train_images.reshape((train_images.shape[0], image_width, image_height, 1))
+test_images = test_images.reshape((test_images.shape[0], image_width, image_height, 1))
+
+# Create the model
+model = models.Sequential()
+# Convolution window size 3x3. Depth of layer 32
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(image_width, image_height, 1)))
+model.add(layers.MaxPooling2D((2, 2)))
+# Convolution window size 3x3. Depth of layer 64
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+# Convolution window size 3x3. Depth of layer 64
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+# Flatten the last convolution layer into a vecor so we can feed this to
+# a regular (dense) neural network layer.
+model.add(layers.Flatten())
+# Fully connected regular neurons (64 in count). Activation function is relu
+model.add(layers.Dense(64, activation='relu'))
+# Fully connected regular neurons (10 in count, one for each class). 
+# Activation function is softmax since we are doing multi-class classification
+model.add(layers.Dense(10, activation='softmax'))
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Start training              
+model.fit(train_images, train_labels, epochs=3)
+
+# Evaluate the model
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print("Accuracy:", test_acc)
