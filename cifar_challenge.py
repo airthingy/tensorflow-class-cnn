991c99e71fd4289aec9a869393f126c3c6467cc9
bibhas <bibhas2@gmail.com>
2019-08-14 15:20:11 -0400

    Save checkpoint and calculate accuracy

--- cifar_challenge.py
+++ cifar_challenge.py
@@ -24,16 +24,16 @@ def build_model(image_height, image_width, image_depth, num_classes):
 
     # three convolutional layers with their channel counts, and a
     # fully connected layer (tha last layer has 10 softmax neurons)
-    K = 40  # first convolutional layer output depth
-    L = 20  # second convolutional layer output depth
-    M = 10  # third convolutional layer
-    N = 65  # Count of neurons in fully connected layer
+    K = 7  # first convolutional layer output depth
+    L = 5  # second convolutional layer output depth
+    M = 5  # third convolutional layer
+    N = 35  # Count of neurons in fully connected layer
 
     # The model
     layer = conv_util.conv_layer(X, K, 3, 1)
-    layer = conv_util.max_pool_layer(layer, 2, 2)
+    layer = conv_util.max_pool_layer(layer, 3, 3)
     layer = conv_util.conv_layer(layer, L, 3, 1)
-    layer = conv_util.max_pool_layer(layer, 2, 2)
+    layer = conv_util.max_pool_layer(layer, 3, 3)
     layer = conv_util.conv_layer(layer, M, 3, 1)
     layer = conv_util.fully_connected_layer(layer, N)
     logits, Y = conv_util.readout_layer(layer, num_classes)
@@ -57,9 +57,11 @@ def train():
     X, Y_, accuracy, Y, optimizer, learning_rate = build_model(image_height, image_width, image_depth, num_classes)
 
     with tf.Session() as sess: 
+        saver = tf.train.Saver()
+
         sess.run(tf.global_variables_initializer())
 
-        num_epochs = 5
+        num_epochs = 50
         batch_size = 150 
         batch_X = np.array_split(x_train, batch_size)
         batch_Y = np.array_split(y_train, batch_size)
@@ -70,6 +72,31 @@ def train():
 
                 if batch % 100 == 0:
                     a = sess.run(accuracy, {X: batch_X[batch], Y_: batch_Y[batch]})
-                    print("Accuracy:", a)
+                    print("Accuracy:", a * 100.0, "%")
+                    saver.save(sess, "./model.ckpt")
 
-train()
+def validate():
+    _, (x_test, y_test) = load_data()
+
+    #Use the first image to get its dimensions
+    image = x_test[0]
+    image_height = image.shape[0]
+    image_width = image.shape[1]
+    image_depth = image.shape[2]
+
+    #Use the first label to get the number of classes.
+    num_classes = y_test[0].shape[0]
+
+    X, Y_, accuracy, Y, optimizer, learning_rate = build_model(image_height, image_width, image_depth, num_classes)
+
+    with tf.Session() as sess: 
+        sess.run(tf.global_variables_initializer())
+
+        saver = tf.train.Saver()
+        # Load the weights and biases
+        saver.restore(sess, "./model.ckpt")
+        # predictions = sess.run(Y, {X: x_test})
+        a = sess.run(accuracy, {X: x_test, Y_: y_test})
+        print("Accuracy:", a * 100.0, "%")
+
+validate()
